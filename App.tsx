7beaf8ca50bf55d665eb7e58b10d4acf8fce61ac
mokshecodes <162047878+mokshecodes@users.noreply.github.com>
2024-05-10 12:35:47 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import Typewriter from "react-ts-typewriter"
+function App() {
+  return (
+    <div>
+      <h1>I am a {" "}
+        <Typewriter text={"Software developer"}/>
+      </h1>
+    </div>
+  )
+}
+
+export default App
